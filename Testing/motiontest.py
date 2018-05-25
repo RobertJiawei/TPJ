e585940209aba4f446e76883169c240b1f8243e1
SQZHENG <22667315+SQZHENG@users.noreply.github.com>
2018-05-25 11:22:37 -0400

    Update motiontest.py

--- Testing/motiontest.py
+++ Testing/motiontest.py
@@ -3,23 +3,15 @@
 import RPi.GPIO as GPIO
 import time
 
-__author__ = "gus-pimylifeup"
-__version__ = "1.0"
-__maintainer__ = "pimylifeup.com"
-
-pir_sensor = 11
-
+motionFeedback_pinOut = 3                       # GPIO 2
 GPIO.setmode(GPIO.BOARD)
+GPIO.setup(motionFeedback_pinOut, GPIO.IN)      # GPIO 2 to be an input
 
-GPIO.setup(pir_sensor, GPIO.IN)
-
-current_state = 0
 times = 0
 try:
     while True:
         time.sleep(2)
-        current_state = GPIO.input(pir_sensor)
-        if current_state == 1:
+        if GPIO.input(motionFeedback_pinOut):
             times+=1
             print("motion No.%s" % (times))
 except KeyboardInterrupt:
