c9e4324c82c783d84730825b35d6d44cf33e134a
RobertJiawei <robertwu1996@gmail.com>
2018-05-25 11:49:22 -0400

    new

--- Testing/motiontest.py
+++ Testing/motiontest.py
@@ -3,7 +3,7 @@
 import RPi.GPIO as GPIO
 import time
 
-motionFeedback_pinOut = 3                       # GPIO 2
+motionFeedback_pinOut = 11                       # GPIO 2
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(motionFeedback_pinOut, GPIO.IN)      # GPIO 2 to be an input
 
