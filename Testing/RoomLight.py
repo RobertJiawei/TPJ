704d9f9403f5bf66c10102e58a1b37129e60e4b2
Robert Wu <robertwu1996@gmail.com>
2018-06-01 12:44:33 -0400

    Update RoomLight.py

--- Testing/RoomLight.py
+++ Testing/RoomLight.py
@@ -3,10 +3,11 @@ import RPi.GPIO as GPIO
 
 def setUp():
 	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(11,GPIO.OUT)
+	GPIO.setup(3,GPIO.OUT)
 	        
 	
 def LED1(state):
 	if state: // True
-		
+		GPIO.output(led_pin, 1)	
 	else:
+		GPIO.output(led_pin, 0)	
