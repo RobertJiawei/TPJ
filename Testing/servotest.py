b4ebff3453966d63de65113cb3a55df7e713834b
RobertJiawei <robertwu1996@gmail.com>
2018-06-01 11:25:05 -0400

    new

--- Testing/servotest.py
+++ Testing/servotest.py
@@ -1,20 +1,22 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(22,GPIO.OUT)
-p=GPIO.PWM(22,50)
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(22, GPIO.OUT)
+
+p = GPIO.PWM(22, 50)
+
 p.start(7.5)
+
 try:
         while True:
-                p.ChangeDutyCycle(7.5)
-                print "Left"
-                time.sleep(1)
-                p.ChangeDutyCycle(12.5)
-                print "Center"
-                time.sleep(1)
-                p.ChangeDutyCycle(2.5)
-                print "Right"
-                time.sleep(1)
+		p.ChangeDutyCycle(7.5)  # turn towards 90 degree
+		time.sleep(1) # sleep 1 second
+		p.ChangeDutyCycle(2.5)  # turn towards 0 degree
+		time.sleep(1) # sleep 1 second
+		p.ChangeDutyCycle(12.5) # turn towards 180 degree
+                time.sleep(1) # sleep 1 second 
 except KeyboardInterrupt:
-        p.stop()
+	p.stop()
         GPIO.cleanup()
