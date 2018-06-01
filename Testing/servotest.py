b4762e52965e447ee482ba3e9b149c3521b00e31
RobertJiawei <robertwu1996@gmail.com>
2018-06-01 10:53:16 -0400

    new

--- Testing/servotest.py
+++ Testing/servotest.py
@@ -27,7 +27,7 @@ try:
        while True:
            for x in range(11):
              p.ChangeDutyCycle(control[x])
-             time.sleep(0.03)
+             time.sleep(0.1)
              print x
            
            """for x in range(9,0,-1):
