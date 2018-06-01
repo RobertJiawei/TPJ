aab27122a85da347f3da8c4a246c6e502e4dba04
RobertJiawei <robertwu1996@gmail.com>
2018-06-01 11:28:55 -0400

    new

--- Testing/servotest.py
+++ Testing/servotest.py
@@ -13,8 +13,6 @@ try:
         while True:
 		p.ChangeDutyCycle(7.5)  # turn towards 90 degree
 		time.sleep(1) # sleep 1 second
-		p.ChangeDutyCycle(2.5)  # turn towards 0 degree
-		time.sleep(1) # sleep 1 second
 		p.ChangeDutyCycle(12.5) # turn towards 180 degree
                 time.sleep(1) # sleep 1 second 
 except KeyboardInterrupt:
