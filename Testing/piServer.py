5e068e10857abe0164ccb5499701c4000bf955f3
RobertJiawei <robertwu1996@gmail.com>
2018-06-05 14:51:29 -0400

    new

--- Testing/piServer.py
+++ Testing/piServer.py
@@ -24,14 +24,17 @@ while True:
                 while True:
                         data = ''
                         data = tcpCliSock.recv(BUFSIZE)
+                        print(data)
+                        '''
                         if not data:
                                 break
                         if data == ctrCmd[0]:
-                                #RoomLight.LED1(0)
+                                RoomLight.LED1(0)
                                 print('LED1 Off')
                         if data == ctrCmd[1]:
-                                #RoomLight.LED1(1)
+                                RoomLight.LED1(1)
                                 print('LED1 On')
+                                '''
         except KeyboardInterrupt:
                 GPIO.cleanup()
                 
