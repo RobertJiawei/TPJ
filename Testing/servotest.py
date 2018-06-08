283d9e41ed0c0a037afaab150ddd2f3cc2d1d957
RobertJiawei <robertwu1996@gmail.com>
2018-06-08 14:35:09 -0400

    new

--- Testing/servotest.py
+++ Testing/servotest.py
@@ -4,17 +4,19 @@ import time
 def setup():
 	GPIO.setmode(GPIO.BOARD)
 	GPIO.setup(12, GPIO.OUT)
-	p = GPIO.PWM(12, 50)
-	p.start(2.5)
+	#p = GPIO.PWM(12, 50)
+	#p.start(2.5)
 
 def leftturn():
 	p = GPIO.PWM(12, 50)
+	p.start(2.5)
 	p.ChangeDutyCycle(2.5)  # turn towards 90 degree
 	print("left")
 	time.sleep(1) # sleep 1 second
 
 def rightturn():
 	p = GPIO.PWM(12, 50)
+	p.start(2.5)
 	p.ChangeDutyCycle(12.5)  # turn towards 90 degree
 	print("right")
 	time.sleep(1) # sleep 1 second
