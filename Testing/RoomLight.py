882925a02b0fd2bd43714bacd719d32dd3c7daab
RobertJiawei <robertwu1996@gmail.com>
2018-06-08 11:57:05 -0400

    new

--- Testing/RoomLight.py
+++ Testing/RoomLight.py
@@ -4,7 +4,14 @@ import RPi.GPIO as GPIO
 def setup():
 	GPIO.setmode(GPIO.BOARD)
 	GPIO.setup(3,GPIO.OUT)
-	        
-	
+	GPIO.setup(5,GPIO.OUT)
+	GPIO.setup(7,GPIO.OUT)
+	        	
 def LED1(state):
-	GPIO.output(3, state)	
+	GPIO.output(3, state)
+
+def LED2(state):
+	GPIO.output(5, state)
+
+def LED3(state):
+	GPIO.output(7, state)	
--- Testing/piServer.py
+++ Testing/piServer.py
@@ -24,17 +24,24 @@ while True:
                 #while True:
                 data = tcpCliSock.recv(BUFSIZE)
                 print(data[2:])
-                time.sleep(1)
-                '''
-                if not data:
-                        break
-                if data == ctrCmd[0]:
-                        RoomLight.LED1(0)
-                        print('LED1 Off')
-                if data == ctrCmd[1]:
+                if data[2:] == "room1on":
                         RoomLight.LED1(1)
-                        print('LED1 On')                        
-                '''
+                        print("ROOM 1 ON!")
+                elif data[2:] == "room1off":
+                        RoomLight.LED1(0)
+                        print("ROOM 1 OFF")
+                elif data[2:] == "room2on":
+                        RoomLight.LED2(1)
+                        print("ROOM 2 ON!")
+                elif data[2:] == "room2off":
+                        RoomLight.LED2(0)
+                        print("ROOM 2 OFF")
+                elif data[2:] == "room3on":
+                        RoomLight.LED3(1)
+                        print("ROOM 3 ON!")
+                elif data[2:] == "room3off":
+                        RoomLight.LED3(0)
+                        print("ROOM 3 OFF")
         except KeyboardInterrupt:
                 GPIO.cleanup()
                 
