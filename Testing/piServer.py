99d4efa1083a361116a59f49f623e7187b8dae06
RobertJiawei <robertwu1996@gmail.com>
2018-06-27 16:24:47 -0400

    new

--- Testing/piServer.py
+++ Testing/piServer.py
@@ -54,9 +54,9 @@ while True:
                         print("WINDOW CLOSING")
                 elif cmd[10:-1] == ctrCmd[8]:
                         os.system("raspivid -o - -t 0 -hf -w 800 -h 400 -fps 24 |cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:1235}' :demux=h264 &")
-                        if cmd[10:-1] == ctrCmd[9]:
-                                print(data)
-                                tcpSerSock.close()
+                elif cmd[10:-1] == ctrCmd[9]:
+                        print(data)
+                        tcpSerSock.close()
         except KeyboardInterrupt:
                 GPIO.cleanup()
                 
