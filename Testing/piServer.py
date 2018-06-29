65421d553a03a6d4248f07a2ebf0731782fa7299
RobertJiawei <robertwu1996@gmail.com>
2018-06-29 13:49:52 -0400

    new

--- Testing/piServer.py
+++ Testing/piServer.py
@@ -58,7 +58,7 @@ while True:
                         print("WINDOW CLOSING")
                 elif cmd[10:-1] == ctrCmd[8]:
                         #os.system("raspivid -o - -t 0 -hf -w 800 -h 400 -fps 24 |cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:1235}' :demux=h264 &")
-                        proc = subprocess.Popen("raspivid -o - -t 0 -hf -w 800 -h 400 -fps 24 |cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:1235}' :demux=h264 &")
+                        proc = subprocess.Popen(["raspivid -o - -t 0 -hf -w 800 -h 400 -fps 24 |cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:1235}' :demux=h264 &"], shell=True)
                         if cmd[10:-1] == ctrCmd[9]:
                                 print(data)
                                 proc.terminate()
