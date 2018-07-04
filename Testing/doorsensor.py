7a5384c626155aac3ed3682c9be4c355efb10d27
RobertJiawei <robertwu1996@gmail.com>
2018-07-04 12:30:31 -0400

    new

---
+++ Testing/doorsensor.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO
+
+doorsensor_pin = 18
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(doorsensor_pin,GPIO.IN)
+
+while True:
+    if GPIO.input(doorsensor_pin):
+        print("Door is closed")
+    else:
+        print("Door is opened")
