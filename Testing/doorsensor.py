1bf7338bc7332b92af57b0643f57d4ce0a53f558
RobertJiawei <robertwu1996@gmail.com>
2018-07-04 13:16:08 -0400

    new

--- Testing/doorsensor.py
+++ Testing/doorsensor.py
@@ -16,6 +16,6 @@ while True:
         GPIO.output(buzzer_pin,1)
         time.sleep(0.3)
     else:
-        GPIO.output(buzzer_pin,0)
+        GPIO.output(buzzer_pin,1)
         print("Door is closed")
     time.sleep(0.1)
