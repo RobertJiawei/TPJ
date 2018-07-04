87010bd81d30c00f716d82d5fc2b42d033918be6
RobertJiawei <robertwu1996@gmail.com>
2018-07-04 13:06:47 -0400

    new

--- Testing/doorsensor.py
+++ Testing/doorsensor.py
@@ -2,13 +2,17 @@ import RPi.GPIO as GPIO
 import time
 
 doorsensor_pin = 12
-
+buzzer_pin = 40
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(doorsensor_pin,GPIO.IN,GPIO.PUD_UP)
 
 while True:
     if GPIO.input(doorsensor_pin):
         print("Door is opened")
+        GPIO.output(buzzer_pin,0)
+        time.sleep(0.3)
+        GPIO.output(buzzer_pin,1)
+        time.sleep(0.3)
     else:
         print("Door is closed")
-    time.sleep(0.5)
+    time.sleep(0.1)
