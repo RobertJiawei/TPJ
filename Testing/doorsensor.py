4d49e01457775cedb5d415aec19fff2d688fc47e
RobertJiawei <robertwu1996@gmail.com>
2018-07-04 13:07:38 -0400

    new

--- Testing/doorsensor.py
+++ Testing/doorsensor.py
@@ -3,8 +3,10 @@ import time
 
 doorsensor_pin = 12
 buzzer_pin = 40
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(doorsensor_pin,GPIO.IN,GPIO.PUD_UP)
+GPIO.setup(buzzer_pin,GPIO.OUT)
 
 while True:
     if GPIO.input(doorsensor_pin):
