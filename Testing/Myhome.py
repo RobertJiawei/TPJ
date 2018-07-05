69a1906a546ff0ffda473922f6678da516754f11
RobertJiawei <robertwu1996@gmail.com>
2018-07-05 16:21:12 -0400

    new

--- Testing/Myhome.py
+++ Testing/Myhome.py
@@ -25,7 +25,7 @@ Doorsensor.setup()
 Window.setup()
 
 ctrCmd = ['1true', '1false', '2true', '2false', '3true',
-              '3false', 'windowtrue', 'windowfalse', 'v', 'vt']
+              '3false', 'wtrue', 'wfalse', 'v', 'vt']
 
 HOST = '192.168.43.5'
 PORT = 1234
