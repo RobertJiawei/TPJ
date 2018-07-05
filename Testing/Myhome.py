12aea0a4b14f30a1815b766d58282647d32a3d78
RobertJiawei <robertwu1996@gmail.com>
2018-07-05 15:46:11 -0400

    new

--- Testing/Myhome.py
+++ Testing/Myhome.py
@@ -4,21 +4,19 @@ import Window as window
 from socket import *
 import RPi.GPIO as GPIO
 import Doorsensor
-from threading import Thread
+import threading
 import time
 
 
-def doorcheck(clisock):
+def doorcheck():
     while True:
         if GPIO.input(12):
             print("Door opened")
             Doorsensor.buzzeron()
-            clisock.send(0)
             while GPIO.input(12):
                 print("Door opened")
         else:
             print("Door is closed")
-            clisock.send(1)
         time.sleep(1)
 
 
@@ -38,56 +36,55 @@ tcpSerSock = socket(AF_INET, SOCK_STREAM)
 tcpSerSock.bind(ADDR)
 tcpSerSock.listen(10)
 
-#threads = []
-#tdoor = threading.Thread(target=doorcheck)
-#threads.append(tdoor)
+threads = []
+tdoor = threading.Thread(target=doorcheck)
+threads.append(tdoor)
 
-"""for t in threads:
+for t in threads:
     t.setDaemon(True)
-    t.start()"""
+    t.start()
 
-while True:
-    print('Waiting for connection')
-    tcpCliSock, addr = tcpSerSock.accept()
-    print('...connected from :', addr)
-    Thread(target=doorcheck, args=tcpCliSock).start()
+    while True:
+        print('Waiting for connection')
+        tcpCliSock, addr = tcpSerSock.accept()
+        print('...connected from :', addr)
 
-    try:
-        data = tcpCliSock.recv(BUFSIZE)
-        cmd = str(data)
-        print(cmd[10:-1])
-        if cmd[10:-1] == ctrCmd[0]:
-            RoomLight.LED1(1)
-            print("ROOM 1 ON!")
-        elif cmd[10:-1] == ctrCmd[1]:
-            RoomLight.LED1(0)
-            print("ROOM 1 OFF")
-        elif cmd[10:-1] == ctrCmd[2]:
-            RoomLight.LED2(1)
-            print("ROOM 2 ON!")
-        elif cmd[10:-1] == ctrCmd[3]:
-            RoomLight.LED2(0)
-            print("ROOM 2 OFF")
-        elif cmd[10:-1] == ctrCmd[4]:
-            RoomLight.LED3(1)
-            print("ROOM 3 ON!")
-        elif cmd[10:-1] == ctrCmd[5]:
-            RoomLight.LED3(0)
-            print("ROOM 3 OFF")
-        elif cmd[8:-1] == ctrCmd[6]:
-            window.leftturn()
-            print("WINDOW OPENING")
-        elif cmd[10:-1] == ctrCmd[7]:
-            window.rightturn()
-            print("WINDOW CLOSING")
-        elif cmd[10:-1] == ctrCmd[8]:
-            os.system(
-                "raspivid -o - -t 0 -hf -w 800 -h 400 -fps 24 |cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:1235}' :demux=h264 &")
-        elif cmd[10:-1] == ctrCmd[9]:
-            print(data)
-            os.system("pkill raspivid")
+        try:
+            data = tcpCliSock.recv(BUFSIZE)
+            cmd = str(data)
+            print(cmd[10:-1])
+            if cmd[10:-1] == ctrCmd[0]:
+                RoomLight.LED1(1)
+                print("ROOM 1 ON!")
+            elif cmd[10:-1] == ctrCmd[1]:
+                RoomLight.LED1(0)
+                print("ROOM 1 OFF")
+            elif cmd[10:-1] == ctrCmd[2]:
+                RoomLight.LED2(1)
+                print("ROOM 2 ON!")
+            elif cmd[10:-1] == ctrCmd[3]:
+                RoomLight.LED2(0)
+                print("ROOM 2 OFF")
+            elif cmd[10:-1] == ctrCmd[4]:
+                RoomLight.LED3(1)
+                print("ROOM 3 ON!")
+            elif cmd[10:-1] == ctrCmd[5]:
+                RoomLight.LED3(0)
+                print("ROOM 3 OFF")
+            elif cmd[8:-1] == ctrCmd[6]:
+                window.leftturn()
+                print("WINDOW OPENING")
+            elif cmd[10:-1] == ctrCmd[7]:
+                window.rightturn()
+                print("WINDOW CLOSING")
+            elif cmd[10:-1] == ctrCmd[8]:
+                os.system(
+                    "raspivid -o - -t 0 -hf -w 800 -h 400 -fps 24 |cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:1235}' :demux=h264 &")
+            elif cmd[10:-1] == ctrCmd[9]:
+                print(data)
+                os.system("pkill raspivid")
 
-    except KeyboardInterrupt:
-        GPIO.cleanup()
+        except KeyboardInterrupt:
+            GPIO.cleanup()
 
-tcpSerSock.close()
+    tcpSerSock.close()
