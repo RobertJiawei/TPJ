c4bacb0ffd88edcf628fcbf343dbb0471616a0ba
RobertJiawei <robertwu1996@gmail.com>
2018-07-08 13:04:19 -0400

    new

--- Testing/Myhome.py
+++ Testing/Myhome.py
@@ -100,11 +100,11 @@ while True:
     if GPIO.input(11):
         # print("Door opened")
         Doorsensor.buzzeron()
-        conn.send("open".encode('utf-8'))
+        conn.sendall("open".encode('utf-8'))
         while GPIO.input(11):
             pass
     else:
-        conn.send("close".encode('utf-8'))
+        conn.sendall("close".encode('utf-8'))
         # print("Door is closed")
     time.sleep(1)
 
