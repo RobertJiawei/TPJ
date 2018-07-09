ebd92ea305853da3cab9560bd181f80962ccb303
RobertJiawei <robertwu1996@gmail.com>
2018-07-09 15:47:07 -0400

    new

--- Testing/Myhome.py
+++ Testing/Myhome.py
@@ -12,7 +12,7 @@ import motiontest
 
 def doorcheck():
     while True:
-        conn, addr = tcpSerSock.accept()
+        #conn, addr = tcpSerSock.accept()
         if GPIO.input(11):
             # print("Door opened")
             Doorsensor.buzzeron()
@@ -29,10 +29,50 @@ def doorcheck():
 
 def motioncheck():
     while True:
-        if GPIO.input(13):
-            print("Yes")
-        else:
-            print("No")
+        print('Waiting for connection')
+        #conn, addr = tcpSerSock.accept()
+        print('...connected from :', addr)
+
+        try:
+            data = conn.recv(BUFSIZE)
+            cmd = str(data)
+            print(cmd[10:-1])
+            if cmd[10:-1] == ctrCmd[0]:
+                RoomLight.LED1(1)
+                print("ROOM 1 ON!")
+            elif cmd[10:-1] == ctrCmd[1]:
+                RoomLight.LED1(0)
+                print("ROOM 1 OFF")
+            elif cmd[10:-1] == ctrCmd[2]:
+                RoomLight.LED2(1)
+                print("ROOM 2 ON!")
+            elif cmd[10:-1] == ctrCmd[3]:
+                RoomLight.LED2(0)
+                print("ROOM 2 OFF")
+            elif cmd[10:-1] == ctrCmd[4]:
+                RoomLight.LED3(1)
+                print("ROOM 3 ON!")
+            elif cmd[10:-1] == ctrCmd[5]:
+                RoomLight.LED3(0)
+                print("ROOM 3 OFF")
+            elif cmd[10:-1] == ctrCmd[6]:
+                Window.leftturn()
+                print("WINDOW OPENING")
+            elif cmd[10:-1] == ctrCmd[7]:
+                Window.rightturn()
+                print("WINDOW CLOSING")
+            elif cmd[10:-1] == ctrCmd[10]:
+                print("Door can be open now!!!!!!")
+                Doorlock.opendoor()
+            elif cmd[10:-1] == ctrCmd[8]:
+                os.system(
+                    "raspivid -o - -t 0 -hf -w 800 -h 400 -fps 24 |cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:1235}' :demux=h264 &")
+            elif cmd[10:-1] == ctrCmd[9]:
+                print(data)
+                os.system("pkill raspivid")
+
+        except KeyboardInterrupt:
+            GPIO.cleanup()
 
 
 RoomLight.setup()
@@ -53,60 +93,20 @@ tcpSerSock = socket(AF_INET, SOCK_STREAM)
 tcpSerSock.bind(ADDR)
 tcpSerSock.listen(10)
 
+conn, addr = tcpSerSock.accept()
+
 threads = []
 tdoor = threading.Thread(target=doorcheck)
-# tmotion = threading.Thread(target=motioncheck)
+tmotion = threading.Thread(target=motioncheck)
 threads.append(tdoor)
-# threads.append(tmotion)
+threads.append(tmotion)
 
 for t in threads:
     t.setDaemon(True)
     t.start()
 
 while True:
-    print('Waiting for connection')
     conn, addr = tcpSerSock.accept()
-    print('...connected from :', addr)
 
-    try:
-        data = conn.recv(BUFSIZE)
-        cmd = str(data)
-        print(cmd[10:-1])
-        if cmd[10:-1] == ctrCmd[0]:
-            RoomLight.LED1(1)
-            print("ROOM 1 ON!")
-        elif cmd[10:-1] == ctrCmd[1]:
-            RoomLight.LED1(0)
-            print("ROOM 1 OFF")
-        elif cmd[10:-1] == ctrCmd[2]:
-            RoomLight.LED2(1)
-            print("ROOM 2 ON!")
-        elif cmd[10:-1] == ctrCmd[3]:
-            RoomLight.LED2(0)
-            print("ROOM 2 OFF")
-        elif cmd[10:-1] == ctrCmd[4]:
-            RoomLight.LED3(1)
-            print("ROOM 3 ON!")
-        elif cmd[10:-1] == ctrCmd[5]:
-            RoomLight.LED3(0)
-            print("ROOM 3 OFF")
-        elif cmd[10:-1] == ctrCmd[6]:
-            Window.leftturn()
-            print("WINDOW OPENING")
-        elif cmd[10:-1] == ctrCmd[7]:
-            Window.rightturn()
-            print("WINDOW CLOSING")
-        elif cmd[10:-1] == ctrCmd[10]:
-            print("Door can be open now!!!!!!")
-            Doorlock.opendoor()
-        elif cmd[10:-1] == ctrCmd[8]:
-            os.system(
-                "raspivid -o - -t 0 -hf -w 800 -h 400 -fps 24 |cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:1235}' :demux=h264 &")
-        elif cmd[10:-1] == ctrCmd[9]:
-            print(data)
-            os.system("pkill raspivid")
-
-    except KeyboardInterrupt:
-        GPIO.cleanup()
 
 tcpSerSock.close()
