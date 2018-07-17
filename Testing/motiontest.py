3c349e93d91fd39d7763f43f4f5f6dd77c8a49ed
RobertJiawei <robertwu1996@gmail.com>
2018-07-17 14:14:00 -0400

    new

--- Testing/motiontest.py
+++ Testing/motiontest.py
@@ -1,29 +1,27 @@
 from socket import *
 import RPi.GPIO as GPIO
 
+led_pin = 3
+senserOut_pin = 13
 
-def setup():
-    led_pin = 3
-    senserOut_pin = 13
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(led_pin, GPIO.OUT)
+GPIO.setup(senserOut_pin, GPIO.IN)
 
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(led_pin, GPIO.OUT)
-    GPIO.setup(senserOut_pin, GPIO.IN)
+HOST = '192.168.43.5'
+PORT = 1237
+ADDR = (HOST, PORT)
 
-    HOST = '192.168.43.5'
-    PORT = 1237
-    ADDR = (HOST, PORT)
+tcpSerSock = socket(AF_INET, SOCK_STREAM)
+tcpSerSock.bind(ADDR)
+tcpSerSock.listen(5)
 
-    tcpSerSock = socket(AF_INET, SOCK_STREAM)
-    tcpSerSock.bind(ADDR)
-    tcpSerSock.listen(5)
-
-    while True:
-        conn, addr = tcpSerSock.accept()
-        if GPIO.input(senserOut_pin):
-            print("Yes")
-            conn.send("Yes".encode())
-        else:
-            print("No")
-            conn.send("No".encode())
+while True:
+    conn, addr = tcpSerSock.accept()
+    if GPIO.input(senserOut_pin):
+        print("Yes")
+        conn.send("Yes".encode())
+    else:
+        print("No")
+        conn.send("No".encode())
 
