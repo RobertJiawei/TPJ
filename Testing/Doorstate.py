e72a481ee30a150feaa00fb242bb05f3315d4af5
RobertJiawei <robertwu1996@gmail.com>
2018-07-17 14:08:42 -0400

    new

--- Testing/Doorstate.py
+++ Testing/Doorstate.py
@@ -4,34 +4,33 @@ import Doorsensor
 import time
 
 
-def setup():
-    Doorsensor.setup()
+Doorsensor.setup()
 
-    HOST = '192.168.43.5'
-    PORT = 1236
-    ADDR = (HOST, PORT)
+HOST = '192.168.43.5'
+PORT = 1236
+ADDR = (HOST, PORT)
 
-    tcpSerSock = socket(AF_INET, SOCK_STREAM)
-    tcpSerSock.bind(ADDR)
-    tcpSerSock.listen(5)
+tcpSerSock = socket(AF_INET, SOCK_STREAM)
+tcpSerSock.bind(ADDR)
+tcpSerSock.listen(5)
 
-    while True:
-        print("waiting for connection")
-        conn, addr = tcpSerSock.accept()
-        print("..... connected .....")
+while True:
+    print("waiting for connection")
+    conn, addr = tcpSerSock.accept()
+    print("..... connected .....")
 
-        if GPIO.input(11):
-            print("Door opened")
-            Doorsensor.buzzeron()
-            conn.send("open".encode('utf-8'))
-            print("door open")
-            while GPIO.input(11):
-                pass
-        else:
-            conn.send("close".encode('utf-8'))
+    if GPIO.input(11):
+        print("Door opened")
+        Doorsensor.buzzeron()
+        conn.send("open".encode('utf-8'))
+        print("door open")
+        while GPIO.input(11):
             pass
-            print("Door is closed")
+    else:
+        conn.send("close".encode('utf-8'))
+        pass
+        print("Door is closed")
 
-        print("door!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    print("door!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
-        time.sleep(1)
+    time.sleep(1)
