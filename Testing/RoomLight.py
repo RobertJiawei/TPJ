6c335e002f1b4296d2dd1b9039fdad502d1ea7fb
SQZHENG <22667315+SQZHENG@users.noreply.github.com>
2018-07-25 14:51:51 -0400

    Update RoomLight.py

--- Testing/RoomLight.py
+++ Testing/RoomLight.py
@@ -1,10 +1,10 @@
 import RPi.GPIO as GPIO  # import Raspberry Pi GPIO library as GPIO
 
-def setup(): # set up GPIOs 3, 5, and 7
+def setup(): # set up on-board pins 3, 5, and 7
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(3, GPIO.OUT)  # Setup on board pin#3 as room light 1 control
-    GPIO.setup(5, GPIO.OUT)  # Setup on board pin#5 for room light 2 control
-    GPIO.setup(7, GPIO.OUT)  # Setup on board pin#7 for room light 3 control
+    GPIO.setup(3, GPIO.OUT)  # set on board pin#3 as room light 1 control
+    GPIO.setup(5, GPIO.OUT)  # set on board pin#5 for room light 2 control
+    GPIO.setup(7, GPIO.OUT)  # set on board pin#7 for room light 3 control
 
 def LED1(state):             # turn room light 1 on if state is 1; or off if state is 0
     GPIO.output(3, state)
