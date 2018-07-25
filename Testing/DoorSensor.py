8c363954b6966adef9e4683ac036843d6cf84ff6
SQZHENG <22667315+SQZHENG@users.noreply.github.com>
2018-07-25 14:47:11 -0400

    Update DoorSensor.py

--- Testing/DoorSensor.py
+++ Testing/DoorSensor.py
@@ -1,14 +1,13 @@
 import RPi.GPIO as GPIO  # import Raspberry Pi GPIO library as GPIO
 import time  # import time library
 
-
-def setup():
+def setup(): # set up GPIOs 11 and 40
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(11, GPIO.IN, GPIO.PUD_UP)  # Set up on board pin#11 as door sensor input
     GPIO.setup(40, GPIO.OUT)  # Set up on board pin#40 for buzzer control
 
 
-def buzzeron():
+def buzzeron(): # make the buzzer to produce a 2-beep sound
     GPIO.output(40, 0)  # turn on the buzzer
     time.sleep(0.1)
     GPIO.output(40, 1)  # turn off the buzzer
