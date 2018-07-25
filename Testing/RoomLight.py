cead4afcbd60e0a642e85d45d6e3a7a7c097f413
SQZHENG <22667315+SQZHENG@users.noreply.github.com>
2018-07-25 14:14:29 -0400

    Update RoomLight.py

--- Testing/RoomLight.py
+++ Testing/RoomLight.py
@@ -2,11 +2,11 @@ import RPi.GPIO as GPIO
 
 def setup():
 	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(3,GPIO.OUT)
-	GPIO.setup(5,GPIO.OUT)
-	GPIO.setup(7,GPIO.OUT)
+	GPIO.setup(3,GPIO.OUT) # for room light 1 control
+	GPIO.setup(5,GPIO.OUT) # for room light 2 control
+	GPIO.setup(7,GPIO.OUT) # for room light 3 control
 	        	
-def LED1(state):
+def LED1(state): # turn room light 1 on if state is 1; or off if state is 0
 	GPIO.output(3, state)
 
 def LED2(state):
