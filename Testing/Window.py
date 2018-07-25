332e9ca830d69ce48a8689169f77dce681277eff
SQZHENG <22667315+SQZHENG@users.noreply.github.com>
2018-07-25 14:54:40 -0400

    Update Window.py

--- Testing/Window.py
+++ Testing/Window.py
@@ -1,4 +1,4 @@
-import RPi.GPIO as GPIO    # import Raspberry Pi GPIO library as GPIO
+import RPi.GPIO as GPIO       # import Raspberry Pi GPIO library as GPIO
 
 def setup():                  # set up on-board pin 12
     GPIO.setmode(GPIO.BOARD)
@@ -7,10 +7,10 @@ def setup():                  # set up on-board pin 12
     p = GPIO.PWM(12, 50)      # set up Global p as PWM output and set frequency as 50Hz
     p.start(12.5)             # start PWM output and set initial length at 12.5 milliseconds
 
-def leftturn(): # turn to 90 degree
+def leftturn():               # turn the server motor to 90 degree
     p.ChangeDutyCycle(7.5) 
     print("left")
 
-def rightturn(): # turn to 180 degree
+def rightturn():              # turn the server motor to 180 degree
     p.ChangeDutyCycle(12.5) 
     print("right")
