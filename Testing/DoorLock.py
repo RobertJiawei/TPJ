ab5e1bb8e360a79e2fca028c030e72af095bc963
SQZHENG <22667315+SQZHENG@users.noreply.github.com>
2018-07-25 14:51:01 -0400

    Update DoorLock.py

--- Testing/DoorLock.py
+++ Testing/DoorLock.py
@@ -1,13 +1,11 @@
 import RPi.GPIO as GPIO  # import Raspberry Pi GPIO library as GPIO
 import time  # import time library
 
-
-def setup():
+def setup(): # set up GPIO 8
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(8, GPIO.OUT)  # Setup on board pin#8 as door lock control
-
-
-def opendoor():
+    GPIO.setup(8, GPIO.OUT)  # for door lock control; set GPIO 8 as an output
+    
+def opendoor(): # unlock the door then lock the door in 10s
     GPIO.output(8, 1)  # Set pin high to open the door
     time.sleep(10)
     GPIO.output(8, 0)  # After 10 seconds, the door will automatic close
