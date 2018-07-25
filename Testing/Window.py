9fe3644e6bc1831e56133c8e40c181bda15ef053
SQZHENG <22667315+SQZHENG@users.noreply.github.com>
2018-07-25 14:49:31 -0400

    Update Window.py

--- Testing/Window.py
+++ Testing/Window.py
@@ -1,19 +1,16 @@
 import RPi.GPIO as GPIO    # import Raspberry Pi GPIO library as GPIO
 
-
-def setup():
+def setup():                  # set up GPIO 12
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(12, GPIO.OUT)  # Set up on board 12 as servo motor input
+    GPIO.setup(12, GPIO.OUT)  # set up on board 12 as servo motor input
     global p
-    p = GPIO.PWM(12, 50)      # Set up Global p as PWM output and set frequency as 50Hz
-    p.start(12.5)             # Start PWM output and set initial length at 12.5 milliseconds
+    p = GPIO.PWM(12, 50)      # set up Global p as PWM output and set frequency as 50Hz
+    p.start(12.5)             # start PWM output and set initial length at 12.5 milliseconds
 
-
-def leftturn():
-    p.ChangeDutyCycle(7.5)  # turn towards 90 degree
+def leftturn(): # turn to 90 degree
+    p.ChangeDutyCycle(7.5) 
     print("left")
 
-
-def rightturn():
-    p.ChangeDutyCycle(12.5)  # turn towards 90 degree
+def rightturn(): # turn to 180 degree
+    p.ChangeDutyCycle(12.5) 
     print("right")
