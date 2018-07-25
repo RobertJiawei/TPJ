8589a0cd7b5b176b297a1e13e44f3e6eba4795ae
SQZHENG <22667315+SQZHENG@users.noreply.github.com>
2018-07-25 14:47:20 -0400

    Update DoorSensor.py

--- Testing/DoorSensor.py
+++ Testing/DoorSensor.py
@@ -6,7 +6,6 @@ def setup(): # set up GPIOs 11 and 40
     GPIO.setup(11, GPIO.IN, GPIO.PUD_UP)  # Set up on board pin#11 as door sensor input
     GPIO.setup(40, GPIO.OUT)  # Set up on board pin#40 for buzzer control
 
-
 def buzzeron(): # make the buzzer to produce a 2-beep sound
     GPIO.output(40, 0)  # turn on the buzzer
     time.sleep(0.1)
