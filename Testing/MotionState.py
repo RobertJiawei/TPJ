94751f81b0ba9bed68d2469ad8e7e0ea8ab6d0bd
RobertJiawei <robertwu1996@gmail.com>
2018-07-30 22:48:04 -0400

    new

--- Testing/MotionState.py
+++ Testing/MotionState.py
@@ -1,5 +1,6 @@
 from socket import *       # import every function in socket library
 import RPi.GPIO as GPIO    # import Raspberry Pi GPIO library as GPIO
+import RoomLight           # import RoomLight.py as a library
 
 led_pin = 37               
 senserOut_pin = 13        
@@ -7,6 +8,8 @@ senserOut_pin = 13
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(led_pin, GPIO.OUT)            # set on-board pin 37 as an LED output pin
 GPIO.setup(senserOut_pin, GPIO.IN)       # set on-board pin 13 as the sensor input pin
+
+RoomLight.setup()
 
 HOST = '192.168.43.5'
 PORT = 1237
@@ -22,7 +25,9 @@ while True:
         GPIO.output(led_pin, 1)           # turn on the LED on the door
         print("Yes")
         conn.send("1".encode())           # send "1" back to MyHome application
+        RoomLight.LED4(1)                 # If someone is near the door, the motion light will be on
     else:                                 # sensor is not triggered
         GPIO.output(led_pin, 0)           # turn off the LED
         print("No")
         conn.send("0".encode())           # send "0" back to MyHome
+        RoomLight.LED4(0)                 # If nobody is near the door, the motion light will be off
--- Testing/RoomLight.py
+++ Testing/RoomLight.py
@@ -6,6 +6,7 @@ def setup():                 # set up on-board pins 3, 5, and 7
     GPIO.setup(3, GPIO.OUT)  # set on board pin#3 as room light 1 control
     GPIO.setup(5, GPIO.OUT)  # set on board pin#5 for room light 2 control
     GPIO.setup(7, GPIO.OUT)  # set on board pin#7 for room light 3 control
+    GPIO.setup(10, GPIO.OUT)  # set on board pin#10 for motion light control
 
 
 def LED1(state):             # turn room light 1 on if state is 1; or off if state is 0
@@ -18,3 +19,7 @@ def LED2(state):             # turn room light 2 on if state is 1; or off if sta
 
 def LED3(state):             # turn room light 3 on if state is 1; or off if state is 0
     GPIO.output(7, state)
+
+
+def LED4(state):             # turn motion light on if state is 1 (someone is near the door); or off if state is 0
+    GPIO.output(10, state)
