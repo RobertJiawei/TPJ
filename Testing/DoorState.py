8133d13b98bb9276224b8699fe8f447c61f46239
RobertJiawei <robertwu1996@gmail.com>
2018-07-31 11:23:12 -0400

    new

--- Testing/DoorState.py
+++ Testing/DoorState.py
@@ -14,18 +14,18 @@ tcpSerSock.bind(ADDR)
 tcpSerSock.listen(5)                         # server start to listen up to 4 client request
 
 while True:
-    print("waiting for connection")
+    #print("waiting for connection")
     conn, addr = tcpSerSock.accept()         # accept the request from client and build connection
-    print("..... connected .....")
+    #print("..... connected .....")
 
     if GPIO.input(11):                       # check GPIO#11 logic level. if it is high, the door is opened
-        print("Door opened")
+        #print("Door opened")
         DoorSensor.buzzeron()                # buzzer will make two 'bip' sound when door is opened
         conn.send("1".encode('utf-8'))       # send message "1" back to client
-        print("Door opened")
+        #print("Door opened")
         while GPIO.input(11):                # when door is open, the buzzer only sound one time
             pass                             # the door status will stay open unless the logic level become low
     else:                                    # if it is low, the door is closed
         conn.send("0".encode('utf-8'))   # send message "0" back to client
-        print("Door closed")
+        #print("Door closed")
     time.sleep(1)                            # the time between each check is 1 second
--- Testing/MyHome.py
+++ Testing/MyHome.py
@@ -22,46 +22,46 @@ tcpSerSock.bind(ADDR)
 tcpSerSock.listen(5)                       # Server start to listen up to 4 client request
 
 while True:
-    print('Waiting for connection')
+    #print('Waiting for connection')
     conn, addr = tcpSerSock.accept()      # accept the request from client and build connection
-    print('...connected from :', addr)
+    #print('...connected from :', addr)
 
     try:
         data = conn.recv(BUFSIZE)         # receive and data send from client and check the command within the while loop
         cmd = str(data)
-        print(cmd[10:-1])
+        #print(cmd[10:-1])
         if cmd[10:-1] == ctrCmd[0]:
             RoomLight.LED1(1)
-            print("ROOM 1 ON!")
+            #print("ROOM 1 ON!")
         elif cmd[10:-1] == ctrCmd[1]:
             RoomLight.LED1(0)
-            print("ROOM 1 OFF")
+            #print("ROOM 1 OFF")
         elif cmd[10:-1] == ctrCmd[2]:
             RoomLight.LED2(1)
-            print("ROOM 2 ON!")
+            #print("ROOM 2 ON!")
         elif cmd[10:-1] == ctrCmd[3]:
             RoomLight.LED2(0)
-            print("ROOM 2 OFF")
+            #print("ROOM 2 OFF")
         elif cmd[10:-1] == ctrCmd[4]:
             RoomLight.LED3(1)
-            print("ROOM 3 ON!")
+            #print("ROOM 3 ON!")
         elif cmd[10:-1] == ctrCmd[5]:
             RoomLight.LED3(0)
-            print("ROOM 3 OFF")
+            #print("ROOM 3 OFF")
         elif cmd[10:-1] == ctrCmd[6]:
             Window.leftturn()
-            print("WINDOW OPENING")
+            #print("WINDOW OPENING")
         elif cmd[10:-1] == ctrCmd[7]:
             Window.rightturn()
-            print("WINDOW CLOSING")
+            #print("WINDOW CLOSING")
         elif cmd[10:-1] == ctrCmd[10]:
-            print("Door can be open now!!!!!!")
+            #print("Door can be open now!!!!!!")
             DoorLock.opendoor()
         elif cmd[10:-1] == ctrCmd[8]:
             os.system(
                 "raspivid -o - -t 0 -hf -w 800 -h 400 -fps 24 |cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:1235}' :demux=h264 &")  # start the camera and stream to the destination at port 1235
         elif cmd[10:-1] == ctrCmd[9]:
-            print(data)
+            #print(data)
             os.system("pkill raspivid")  # stop camera streaming
 
     except KeyboardInterrupt:
