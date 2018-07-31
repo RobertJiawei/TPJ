1e064ebc54ec9786cc9bcb080cfa94946115b63f
SQZHENG <22667315+SQZHENG@users.noreply.github.com>
2018-07-31 11:19:17 -0400

    Update Window.py

--- Testing/Window.py
+++ Testing/Window.py
@@ -6,14 +6,14 @@ def setup():                  # set up on-board pin 12
     GPIO.setup(12, GPIO.OUT)  # set up on-board 12 as servo motor input
     global p
     p = GPIO.PWM(12, 50)      # set up Global p as PWM output and set frequency as 50Hz
-    p.start(12.5)             # start PWM output and set initial length at 12.5 milliseconds
+    # p.start(12.5)             # start PWM output and set initial length at 12.5 milliseconds
 
 
 def leftturn():               # turn the server motor to 90 degree
     p.ChangeDutyCycle(7.5) 
-    print("left")
+    # print("left")
 
 
 def rightturn():              # turn the server motor to 180 degree
     p.ChangeDutyCycle(12.5) 
-    print("right")
+    # print("right")
